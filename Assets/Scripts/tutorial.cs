548f1d8afc110feb3acde55f7a4576a0e6b93ee3
Marco Canton <67104790+MarcoCanton1@users.noreply.github.com>
2022-06-10 15:55:40 -0300

    Revision 4

---
+++ Assets/Scripts/tutorial.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial : MonoBehaviour
+{
+    public bool apreto = false;
+    public GameObject pared;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (apreto)
+        {
+            if (pared.transform.position.y < 24.5f)
+            {
+                pared.transform.position += new Vector3(0, 0.2f, 0);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Jugador")
+        {
+            apreto = true;
+        }
+    }
+}
